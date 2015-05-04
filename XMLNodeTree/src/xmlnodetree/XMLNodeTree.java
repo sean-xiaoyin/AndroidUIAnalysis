a3cd4632563f0a12a2163416c9bbe1ea4ba81720
Sean <wxy.6056@gmail.com>
2015-05-04 14:21:05 -0500

    Update XMLNodeTree.java
    extraction of nodes with hashtable

--- XMLNodeTree/src/xmlnodetree/XMLNodeTree.java
+++ XMLNodeTree/src/xmlnodetree/XMLNodeTree.java
@@ -35,41 +35,49 @@ public class XMLNodeTree extends JFrame {
     /**
      * @param args the command line arguments
      */
+     
+    public static Set fetchAttributes(Node node){
+        Set<String> ret = new LinkedHashSet<String>();
+        NamedNodeMap al = node.getAttributes();
+        for (int j = 0; j < al.getLength(); j++) {  // attribute loop
+            Node attr = al.item(j);
+            if (attr.getNodeType() == Node.ATTRIBUTE_NODE && attr.getTextContent().contains("@string") ) {
+                String str = attr.getTextContent().substring(8);
+                ret.add(str);
+            }
+        }
+        return ret;
+    }
     
-    public static Set fetchAllNodes ( Node node, ArrayList<String> t, Hashtable idtext_table) {
+    public static Set fetchAllNodes ( Node node, Hashtable<Node, Set<String>> t) {
             
         Set<String> s = new LinkedHashSet<>();  // attributes for one node
         
         if ( node.hasChildNodes() == false )  { // has not childnode = leaf
             if (node.getNodeType() == Node.ELEMENT_NODE) {   
-                NamedNodeMap al = node.getAttributes();
-                for (int j = 0; j < al.getLength(); j++) {  // attribute loop
-                    Node attr = al.item(j);
-                    if (attr.getNodeType() == Node.ATTRIBUTE_NODE && attr.getTextContent().contains("@string") ) {
-                        String str = idtext_table.get(attr.getTextContent().substring(8)).toString();
-                        s.add(str);
-                    }
-                }              
+                s.addAll(fetchAttributes(node))
+                t.put(node, s);
             }    
         }
-        else { // has childnode
-            NodeList childlist = node.getChildNodes();
-            for ( int i = 0; i < childlist.getLength(); i++) {
-                Node c = childlist.item(i);
-                if (c.getNodeType() == Node.ELEMENT_NODE) {
-                    Set<String> stmp = fetchAllNodes(c, t, idtext_table);
-                    s.addAll(stmp);
-                    System.out.println(c.getNodeName()+ "~"+ stmp.toString());
-                    t.add(c.getNodeName()+ "~"+ stmp.toString());
-    
+        else {
+            if (node.getNodeType() == Node.ELEMENT_NODE) { 
+                s.addAll(fetchAttributes(node))  
+
+                // has childnode
+                NodeList childlist = node.getChildNodes();
+                for ( int i = 0; i < childlist.getLength(); i++) {
+                    Node c = childlist.item(i);
+                    if (c.getNodeType() == Node.ELEMENT_NODE) {
+                        Set<String> stmp = fetchAllNodes(c, t, idtext_table);
+                        s.addAll(stmp);
+                        System.out.println(c.getNodeName()+ "~"+ stmp.toString());
+//                      t.add(c.getNodeName()+ "~"+ stmp.toString());
+                    }
                 }
-                
+                t.put(node, s);
             }
-            
         }
-        
-    return s;    
-    
+        return s;
     }
     
     public static Hashtable HashTextID ( Document doc ) { //Hashtable for text and id
